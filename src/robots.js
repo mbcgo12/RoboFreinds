f8a261badf6cce2f265533d554817f17de8e74ab
mbcgo12 <76557695+mbcgo12@users.noreply.github.com>
2021-07-01 11:41:58 +0900

    push1

---
+++ src/robots.js
@@ -0,0 +1,64 @@
+export const robots = [
+
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email:'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email:'shanna@elissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Antonette',
+        email:'Naksajda@mdkd.net'
+    },
+    {
+        id: 4,
+        name: 'BBBBBBBB Bauch',
+        username: 'Antonette',
+        email:'Naksajda@mdkd.net'
+    },
+    {
+        id: 5,
+        name: 'CCCCCCCCC Bauch',
+        username: 'Antonette',
+        email:'Naksajda@mdkd.net'
+    },
+    {
+        id: 6,
+        name: 'DDDDDDDDD Bauch',
+        username: 'Antonette',
+        email:'Naksajda@mdkd.net'
+    },
+    {
+        id: 7,
+        name: 'EEEEEEEEE Bauch',
+        username: 'Antonette',
+        email:'Naksajda@mdkd.net'
+    },
+    {
+        id: 8,
+        name: 'FFFFFFFFF Bauch',
+        username: 'Antonette',
+        email:'Naksajda@mdkd.net'
+    },
+    {
+        id: 9,
+        name: 'GGGGGGGGG Bauch',
+        username: 'Antonette',
+        email:'Naksajda@mdkd.net'
+    },
+    {
+        id: 10,
+        name: 'HHHHHHHHH Bauch',
+        username: 'Antonette',
+        email:'Naksajda@mdkd.net'
+    },
+
+]
