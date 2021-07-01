a104ef3a9a4552457ccb5d183e59330e4b186d1f
mbcgo12 <76557695+mbcgo12@users.noreply.github.com>
2021-07-01 13:11:03 +0900

    push2

--- src/App.js
+++ src/App.js
@@ -9,10 +9,17 @@ class App extends Component{
     constructor(){
         super()
         this.state = {
-            robots:robots,
+            robots:[],
             searchfield:''            
-        }
+        }        
     }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(respones=>respones.json())
+        .then(user=>this.setState({ robots: user }))        
+    }
+
     onSearchChange = (event)=>{
         this.setState({searchfield: event.target.value})        
     }
@@ -22,14 +29,17 @@ class App extends Component{
         const filteredRobots = this.state.robots.filter(robots=>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        
-        return(
-            <div className='tc'>
-                <h1 className='f1'>RoboFreinds</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Cardlist robots={ filteredRobots }/>
-            </div>
-        );
+        if(this.state.robots.length===0){
+            return <h1>Loding</h1>
+        }else{         
+            return(
+                <div className='tc'>
+                    <h1 className='f1'>RoboFreinds</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Cardlist robots={ filteredRobots }/>
+                </div>
+            );
+        }        
     }  
 }
 
