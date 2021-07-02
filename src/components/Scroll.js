ced3bdd81fd8677e1dad96bfc3789decd55c6e5c
mbcgo12 <76557695+mbcgo12@users.noreply.github.com>
2021-07-02 11:38:59 +0900

    style border modify

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props)=>{
     return (
-        <div style={{overflowY: 'scroll', border:'5px solid #fff', height:'800px'}}>
+        <div style={{overflowY: 'scroll', border:'5px solid #fff', height:'750px'}}>
             {props.children}
         </div>
     );
