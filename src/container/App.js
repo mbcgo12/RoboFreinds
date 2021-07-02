daba2adb5431f63c8d087c0ebf32bab2d0f2f109
mbcgo12 <76557695+mbcgo12@users.noreply.github.com>
2021-07-02 11:00:31 +0900

    push finish

--- src/container/App.js
+++ src/container/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import Cardlist from './Cardlist';
-import SearchBox from './SearchBox';
-import { robots } from './robots';
+import Cardlist from '../components/Cardlist';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+// import { robots } from './robots';
 import './app.css';
 
 
@@ -25,21 +26,21 @@ class App extends Component{
     }
 
     render(){
-        
-        const filteredRobots = this.state.robots.filter(robots=>{
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots,searchfield } = this.state;
+        const filteredRobots = robots.filter(robot=>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if(this.state.robots.length===0){
-            return <h1>Loding</h1>
-        }else{         
-            return(
+        return !robots.length?
+            <h1>Loding</h1>:
+            (
                 <div className='tc'>
                     <h1 className='f1'>RoboFreinds</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
-                    <Cardlist robots={ filteredRobots }/>
+                    <Scroll>
+                        <Cardlist robots={ filteredRobots }/>
+                    </Scroll>
                 </div>
-            );
-        }        
+            );                
     }  
 }
 
